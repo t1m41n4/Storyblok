b9b1aacc459884e0a43271ec5ad066870dffd5d3
Tim <timmaina64@gmail.com>
2025-08-28 12:09:15 +0300

    Revert Testimonial component to show title and description

--- my-storyblok-demo/components/Testimonial.js
+++ my-storyblok-demo/components/Testimonial.js
@@ -4,8 +4,10 @@ import { storyblokEditable } from "@storyblok/react";
 const Testimonial = ({ blok }) => (
 	<section {...storyblokEditable(blok)} className="flex flex-col items-center justify-center py-8">
 		<div className="bg-gradient-to-r from-purple-100 to-blue-100 rounded-xl shadow-lg p-8 max-w-xl w-full">
-			<blockquote className="italic text-xl text-center text-gray-800 mb-4">“{blok.quote}”</blockquote>
-			<p className="text-right font-semibold text-gray-700">- {blok.author}</p>
+			<h2>{blok.title}</h2>
+			<p>{blok.description}</p>
+			<blockquote>“{blok.quote}”</blockquote>
+			<p>- {blok.author}</p>
 		</div>
 	</section>
 );
